93b6ec70ca304f6db7a4e7c6d877f2eb8237b9e8
renoxvi <vicreno08@gmail.com>
2023-01-19 13:02:39 -0800

    bytes

--- bytecodes/001.m
+++ bytecodes/001.m
@@ -3,11 +3,10 @@ push 1 Push 1 onto the stack
 
 push 2
   push 3
-                   pall    
+                   pall
 
-
-                           
+    
 push 4
 
-    push 5    
-      push    6        
+    push 5
+      push    6
